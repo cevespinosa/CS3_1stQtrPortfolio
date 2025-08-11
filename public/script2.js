8084731f13381f3f61436cef52a3c9c5c2e7deb1
cevespinosa <b2029cevespinosa@pshs.edu.ph>
2025-08-11 12:10:17 +0800

    3rd version

---
+++ public/script2.js
@@ -0,0 +1,11 @@
+if(sure) {
+    let  totalRequired = (Number(numPerDay) * 365) * (Number(maxAge)-Number(age));
+
+    document.write("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
+
+    alert(`You will need ${totalRequired} to last you until the ripe old age of ${maxAge}`)
+console.log ("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
+}
+else {
+    window.alert("Please click run again")
+}
